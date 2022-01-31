e104b91c970c9085fe615ff904f8835d8f88bd20
Yaya-k <43400066+Yaya-k@users.noreply.github.com>
2022-01-31 20:21:05 +0100

    Update import.py

--- import.py
+++ import.py
@@ -21,7 +21,7 @@ from sklearn import model_selection
 from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import accuracy_score
-from sklearn.linear_model import LogisticRegresion
+from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
